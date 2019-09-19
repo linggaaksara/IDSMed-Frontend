8fed7d52d922b3f30437a77ecc69d3caa931a752
linggaaksara <linggaaksara@gmail.com>
2019-09-19 08:40:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Jumbotron, Button} from 'reactstrap';
+import Header from './Header.jsx';
+import Satu from './Satu';
+import Device from './Device';
+import Patients from './Patients';
+import Login from './Login';
+import Inputt from './Inputt';
+import Manage from './Manage';
+import Inman from './Inman';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import PrivateRoute from 'react-private-route';
+// import Not from './Not';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div>
+      <Router>
+        <Route path="/Satu" component={Satu} />
+        <Route path="/Device" component={Device} />
+        <Route path="/Patients" component={Patients} />
+        <Route path="/Login" component={Login} />
+        <Route path="/Inputt" component={Inputt} />
+        <Route path="/Manage" component={Manage} />
+        <Route path="/Inman" component={Inman} />
+       </Router>
+       </div>  
+    ); 
   }
 }
 
